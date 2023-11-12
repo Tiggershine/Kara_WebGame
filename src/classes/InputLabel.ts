208dc792ff327704e67ba72f30c7410d4afd7902
Hyun Kim <kmasterh@gmail.com>
2023-11-12 16:29:13 +0100

    Fix. function for edit  inputlabel

--- src/classes/InputLabel.ts
+++ src/classes/InputLabel.ts
@@ -11,8 +11,6 @@ export class InputLabel extends Phaser.GameObjects.Container {
   private timerEvent?: Phaser.Time.TimerEvent;
   private lastClickTime: number = 0;
   private doubleClickDelay: number = 300;
-  private inputElement!: HTMLInputElement;
-  private newText: string = ''; // 새로운 멤버 변수 추가
 
   constructor(
     scene: Phaser.Scene,
@@ -102,88 +100,111 @@ export class InputLabel extends Phaser.GameObjects.Container {
     this.label.setDepth(2);
 
     scene.add.existing(this);
-
-    this.createInputElement(); // HTML input 요소 생성
   }
 
-  private createInputElement() {
-    // 이미 존재하는 input 요소가 있다면 삭제
-    const existingInput = document.getElementById('phaser-game-input');
-    if (existingInput) {
-      document.body.removeChild(existingInput);
-    }
-
-    // 새 input 요소 생성
-    this.inputElement = document.createElement('input');
-    this.inputElement.type = 'text';
-    this.inputElement.id = 'phaser-game-input';
-    this.inputElement.style.position = 'absolute';
-    this.inputElement.style.bottom = '0';
-    this.inputElement.style.left = '0';
-    this.inputElement.style.opacity = '0'; // 보이지 않게 설정
-
-    // input 요소를 DOM에 추가
-    document.body.appendChild(this.inputElement);
+  // HTML input 요소 생성 및 설정
+  createHiddenInput() {
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.style.position = 'absolute';
+    input.style.opacity = '0'; // 보이지 않게 설정
+    input.style.pointerEvents = 'none'; // 포인터 이벤트 방지
+    document.body.appendChild(input);
+    return input;
   }
 
   private handleDoubleClick = (): void => {
     console.log('LongPress event');
+
+    // HTML input 요소 생성 및 설정
+    const htmlInput = document.createElement('input');
+    htmlInput.type = 'text';
+    htmlInput.style.position = 'absolute';
+    htmlInput.style.opacity = '0'; // 보이지 않게 설정
+    htmlInput.style.left = `${this.x}px`;
+    htmlInput.style.top = `${this.y}px`;
+    document.body.appendChild(htmlInput);
+
+    // input 이벤트 리스너 함수
+    const onInput = (event: Event) => {
+      const target = event.target as HTMLInputElement;
+      this.label.text = this.formatText(target.value) + '|';
+    };
+
+    // HTML input 요소에 이벤트 리스너 추가
+    htmlInput.addEventListener('input', onInput);
+
+    // HTML input 요소에 포커스 설정하여 가상 키보드 활성화
+    htmlInput.focus();
 
     // Store the original text in case no new text is entered
     const originalText = this.label.text;
 
     // Initialize new text input
     // let newText = '';
-    // HTML input 요소 활성화
-    this.inputElement.style.opacity = '1'; // 보이게 설정
-    this.inputElement.value = this.label.text; // 현재 라벨 텍스트를 input 값으로 설정
-    this.inputElement.focus(); // input 요소에 포커스 설정
-
-    this.inputElement.oninput = () => {
-      // input 요소에서 입력된 텍스트를 Phaser 게임으로 가져옴
-      this.newText = this.inputElement.value;
-      this.label.text = this.formatText(this.newText);
-    };
 
     // Change the label's color to indicate editing
     this.label.setColor('#F9A02D');
 
     // Create a blinking cursor effect
+    // const cursorBlink = this.scene.time.addEvent({
+    //   delay: 530,
+    //   callback: () => {
+    //     this.label.text =
+    //       this.formatText(newText) + (this.label.text.endsWith('|') ? '' : '|');
+    //   },
+    //   loop: true,
+    // });
+    // cursorBlink 효과 계속 사용
     const cursorBlink = this.scene.time.addEvent({
       delay: 530,
       callback: () => {
         this.label.text =
-          this.formatText(this.newText) +
+          this.formatText(htmlInput.value) +
           (this.label.text.endsWith('|') ? '' : '|');
       },
       loop: true,
     });
 
     // Remove any existing keyboard listeners to prevent duplicates
-    this.scene.input.keyboard.off('keydown');
+    // this.scene.input.keyboard.off('keydown');
 
-    // Create a keyboard listener for mobile key inputs
-    this.scene.input.keyboard.on('keydown', (event: KeyboardEvent) => {
-      if (event.keyCode === 8 && this.newText.length > 0) {
-        // Backspace
-        this.newText = this.newText.slice(0, -1);
-      } else if (event.key.length === 1 && this.newText.length < 10) {
-        // Other characters
-        this.newText += event.key;
-      }
-      this.label.text = this.formatText(this.newText) + '|';
-      this.label.setOrigin(0.5, 0.5);
-    });
+    // // Create a keyboard listener for mobile key inputs
+    // this.scene.input.keyboard.on('keydown', (event: KeyboardEvent) => {
+    //   if (event.keyCode === 8 && newText.length > 0) {
+    //     // Backspace
+    //     newText = newText.slice(0, -1);
+    //   } else if (event.key.length === 1 && newText.length < 10) {
+    //     // Other characters
+    //     newText += event.key;
+    //   }
+    //   this.label.text = this.formatText(newText) + '|';
+    //   this.label.setOrigin(0.5, 0.5);
+    // });
 
-    // Remove any existing pointerdown listeners to prevent duplicates
-    this.scene.input.off('pointerdown');
+    // // Remove any existing pointerdown listeners to prevent duplicates
+    // this.scene.input.off('pointerdown');
 
-    // Listen for pointerdown events outside this object
+    // // Listen for pointerdown events outside this object
+    // this.scene.input.on(
+    //   'pointerdown',
+    //   (pointer: Phaser.Input.Pointer) => {
+    //     if (!this.getBounds().contains(pointer.x, pointer.y)) {
+    //       this.finishEditing(cursorBlink, newText, originalText);
+    //     }
+    //   },
+    //   this
+    // );
+    // 포인터 다운 이벤트 리스너에서 HTML input 요소 숨기기 및 cursorBlink 중지
     this.scene.input.on(
       'pointerdown',
       (pointer: Phaser.Input.Pointer) => {
         if (!this.getBounds().contains(pointer.x, pointer.y)) {
-          this.finishEditing(cursorBlink, this.newText, originalText);
+          htmlInput.style.display = 'none'; // HTML input 요소 숨기기
+          htmlInput.removeEventListener('input', onInput); // 이벤트 리스너 제거
+          document.body.removeChild(htmlInput); // HTML input 요소 제거
+          cursorBlink.remove(); // cursorBlink 중지
+          this.finishEditing(cursorBlink, htmlInput.value, this.label.text); // 편집 완료 처리
         }
       },
       this
