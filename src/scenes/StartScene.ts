53b28c7e57a52e867154ba361a7f24eea07e1da4
Hyun Kim <kmasterh@gmail.com>
2023-12-04 16:08:26 +0100

    Change order of execution for sound function

--- src/scenes/StartScene.ts
+++ src/scenes/StartScene.ts
@@ -40,10 +40,10 @@ export default class StartScene extends Phaser.Scene {
       this.startBtn.setTexture('startBtn');
     });
     this.startBtn.on('pointerdown', () => {
-      this.sound.play('startButtonSound');
-
       this.cameras.main.fadeOut(300, 0, 0, 0, (_: any, progress: number) => {
         if (progress === 1) {
+          this.sound.play('startButtonSound');
+
           // 페이드 아웃이 완료되면 새 장면 시작
           this.scene.start('MenuScene');
         }
